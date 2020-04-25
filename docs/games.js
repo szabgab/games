4f8fae5f518f36028ae10c9197a14dbd187f5fbc
Gabor Szabo <gabor@szabgab.com>
2020-04-25 18:24:04 +0300

    table, buttons

--- docs/games.js
+++ docs/games.js
@@ -1,4 +1,44 @@
 function start() {
-    alert("Hello World");
+    console.log("start")
+    let width = 3;
+    let height = 4;
+    console.log("Height: ", height) 
+    console.log("Width: ", width) 
+    let board = "<table>";
+
+    for (h=0; h < height; h++) {
+        let row = "<tr>";
+        for (w=0; w < width; w++) {
+            let btnId = h + "-" + w;
+            row += '<td><button class="boardBtn" id="' + btnId + '">1</button></td>';
+        }
+        row += "</tr>\n";
+        board += row;
+    }
+    document.getElementById('board').innerHTML = board;
+
+    var buttons = document.getElementsByClassName('boardBtn');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', move);
+    };
+
 }
-start()
+
+function move(event) {
+    //console.log("move");
+    //console.log(event);
+    console.log(this.id);
+    let parts = this.id.split('-');
+    x = parts[0];
+    y = parts[1];
+    console.log(x);
+    console.log(y);
+}
+
+function setup() {
+    document.getElementById('start').addEventListener('click', start);
+}
+
+
+
+setup()
