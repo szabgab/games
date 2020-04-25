f1f59ba15106659e406ed337c16ca23d272c1730
Gabor Szabo <gabor@szabgab.com>
2020-04-25 22:37:03 +0300

    improved color selector

--- docs/color/games.js
+++ docs/color/games.js
@@ -1,5 +1,9 @@
 "use strict";
 
+let red = 0;
+let green = 0;
+let blue = 0;
+let hidden_color_hex = "#000000";
 
 function toHex (num) { 
     let hex = Number(num).toString(16);
@@ -11,18 +15,35 @@ function toHex (num) {
 
 function start() {
     console.log("start");
-    let red = Math.floor(Math.random() * 256 );
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
+    red = 0;
+    green = 0;
+    blue = 0;
+    if (document.getElementById('use-red').checked) {
+        red = Math.floor(Math.random() * 256 );
+    }
+    if (document.getElementById('use-green').checked) {
+        green = Math.floor(Math.random() * 256);
+    }
+    if (document.getElementById('use-blue').checked) {
+        blue = Math.floor(Math.random() * 256);
+    }
     console.log("Red: ", red);
     console.log("Green: ", green);
     console.log("Blue: ", blue);
 
     let board = "";
-    let color = "#" + toHex(red) + toHex(green) + toHex(blue);
-    console.log(color);
-    document.getElementById("left").style.background = color;
+    hidden_color_hex = "#" + toHex(red) + toHex(green) + toHex(blue);
+    console.log(hidden_color_hex);
+    document.getElementById('hidden-color').innerHTML = "";
+    document.getElementById('match').innerHTML = "";
+
+    document.getElementById("left").style.background = hidden_color_hex;
     setColor();
+}
+
+function show_hidden() {
+    document.getElementById('hidden-color').innerHTML = hidden_color_hex + " (" + red + ", " + green + ", " + blue + ")";
+    // TODO set color
 }
 
 function setColor() {
@@ -32,14 +53,20 @@ function setColor() {
     console.log(userRed);
     let color = "#" + toHex(userRed) + toHex(userGreen) + toHex(userBlue);
     document.getElementById("right").style.background = color;
+//    console.log("red", red)
+//    console.log("user red", userRed)
+    if (userRed == red && userGreen == green && userBlue == blue) {
+        document.getElementById('match').innerHTML = "Match!";
+    }
 }
 
 function setup() {
     document.getElementById('start').addEventListener('click', start);
+    document.getElementById('show').addEventListener('click', show_hidden);
     var buttons = document.getElementsByClassName('colors');
     for (var i=0; i < buttons.length; i++) {
         buttons[i].addEventListener('change', setColor);
-        buttons[i].value = 255;
+        buttons[i].value = 0;
     };
 
 }
